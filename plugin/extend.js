d419a9a6ae71e669723467bedd3d3be6af3d2ed3
guido <guido.porruvecchio@gmail.com>
2017-03-13 10:52:27 +0100

    CKAN plugin update
    - Sign-in now returns also username and CKAN apikey

--- plugin/extend.js
+++ plugin/extend.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var user = require("../models/users").User;
 
 var plugins = [
     {
         "resource": "/users/signup",
         "method": "POST",
         "mode": "before_after",
-        "enabled": false,
+        "enabled": true,
         "params": ["body","ckan_username"],
         "extender": {
             'before': function (req, content, cType, callback) {
+                //checking all parameters have been set
                 var par = ["email", "password"];
                 var ko = false;
                 var user = req.body['user'];
@@ -21,11 +23,11 @@ var plugins = [
                     }
                 }
                 if (ko) {
-                    callback({error: 400, error_message: "Missing parameters " + missing.toString()})
+                    callback({error_code: 400, error_message: "Missing parameters " + missing.toString()})
                 }
                 else {
                     var request = require("request");
-                    var name = user.email.split("@")[0].replace(".", "_");
+                    var name = user.email.split("@")[0].replace(".", "_"); //sanitizing username from email
                     var body = {
                         email: user.email,
                         password: user.password,
@@ -38,16 +40,19 @@ var plugins = [
                         },
                         body: JSON.stringify(body)
                     };
+                    //making actual request to CKAN for user creation
                     request.post(options, function (err,res,cBody) {
                         var b = JSON.parse(cBody);
                         if (b.result && b.result.apikey) {
-                            req.body.user.ckan_apikey = b.result.apikey;
-                            req.ckan_username = name;
+                            req.body.user.ckan_apikey = b.result.apikey; //setting CKAN apikey to user dictionary
+                            req.ckan_username = name;   //appending CKAN username to request, to be used in AFTER section
+                            callback(err, req);
                         }
-                        callback(err, req);
+                        else callback({error_code: 400, error_message: b.error.name[0]});
                     });
                 }
             },
+            //deleting the just created CKAN user in case regular user wasn't correctly created
             'after': function (req, content, cType, callback) {
                 var request = require("request");
                 if (content.error) {
@@ -65,6 +70,32 @@ var plugins = [
                 }
                 else
                     callback(null, content);
+            }
+        }
+    },
+    {
+        "resource": "/users/signin",
+        "method": "POST",
+        "mode": "after",
+        "enabled": true,
+        "params": ["body"],
+        "extender": function (req, content, cType, callback) {
+            if (content.access_credentials) {
+                var userId = content.access_credentials.userId;
+                user.findById(userId, "ckan_apikey email", function (err, foundUser) {
+                    if (err || !foundUser) {
+                        return callback(null, content);
+                    }
+                    else {
+                        content.access_credentials.ckan_apikey = foundUser.ckan_apikey;
+                        var name = foundUser.email.split("@")[0].replace(".", "_");
+                        content.access_credentials.ckan_username = name;
+                        return callback(null, content);
+                    }
+                })
+            }
+            else {
+                return callback(null, content);
             }
         }
     }
