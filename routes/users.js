34bafab0efe1b75b38dfe7420ab1569b66f0019c
Alessandro <a.romanino@gmail.com>
2018-06-11 09:53:58 +0200

    Fix a bug in get user by id
    - fix a bug in get user type from auth(add Json.parse in ressponse)

--- routes/users.js
+++ routes/users.js
@@ -710,6 +710,7 @@ router.get('/:id', [jwtMiddle.decodeToken, middlewares.ensureUserIsAdminOrSelf],
                     request.get(rqparams, function (error, response) {
                         if(error) res.status(500).send({error: 'internal_error', error_message: 'something blew up in get user Type from auth ms, ERROR:' + err});
 
+                        response.body=JSON.parse(response.body);
                         var resultWithType=_.clone(results);
                         resultWithType.type=response.body.type || null;
                         res.send(resultWithType);
